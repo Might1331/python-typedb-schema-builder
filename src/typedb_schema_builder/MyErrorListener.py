49ea124e1d59184e752c06089fcc3c4b5020cd67
Pratap Singh <55887674+Might1331@users.noreply.github.com>
2023-08-21 03:08:23 +0530

    for review

--- src/typedb_schema_builder/MyErrorListener.py
+++
@@ -1,5 +0,0 @@
-from antlr4.error.ErrorListener import ErrorListener
-
-class MyErrorListener(ErrorListener):
-    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        raise Exception(f"error message: {msg}")
