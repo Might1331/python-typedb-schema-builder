27effdc9e0d9a0c18b8a0838e8df09d8008c5272
Might1331 <prataps801@gmail.com>
2023-09-08 01:17:20 +0530

    description now reads readme.md

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="typedb-schema-builder",
-    version="11.0.8",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version="11.0.10",
     description = "typedb schema builder package",
     packages=find_packages(),
     install_requires=[
